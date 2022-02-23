6f5d43322a7dbbae04da145212dcc408eb9902ab
JR-CodeHub <justinkennelly@email.arizona.edu>
2022-02-22 23:47:22 -0700

    init function
    Trying a function to initialize all tables

--- phase2/message.h
+++ phase2/message.h
@@ -1,34 +1,34 @@
 #define DEBUG2 1
 
-typedef struct mail_slot *slot_ptr;
 typedef struct mailbox mail_box;
+
+typedef struct mail_slot mail_slot;
+typedef struct mail_slot *slot_ptr;
+
+typedef struct mbox_proc mbox_proc;
 typedef struct mbox_proc *mbox_proc_ptr;
 
 struct mailbox {
    int           mbox_id;
-   /* other items as needed... */
-// int           num_slots
-// int           max_slot_size
-// int           slots_used
-// mbox_proc_ptr my_slots
-// mbox_proc_ptr blocked_procs
+   int           status;
+   int           num_slots;
+   int           max_slot_size;
+   /* slots; */
+   /* my_slots; */
+   /* blocked_procs; */
 };
 
 struct mail_slot {
    int       mbox_id;
    int       status;
-   /* other items as needed... */
-// char      message[m]
-// mail_slot slot_ptr* next_slot
-
+   char      message[MAX_MESSAGE];
+   slot_ptr  next_slot;
 };
 
-/*
 struct mbox_proc_ptr {
-   int      pid;
-   mbox_proc_ptr* next_ptr
-}
-*/
+   int           pid;
+   mbox_proc_ptr next_ptr;
+};
 
 struct psr_bits {
     unsigned int cur_mode:1;
@@ -42,3 +42,10 @@ union psr_values {
    struct psr_bits bits;
    unsigned int integer_part;
 };
+
+#define USED 1
+#define UNUSED 0
+
+#define MAILBOX_TABLE 1
+#define SLOT_TABLE    2
+#define PROC_TABLE    3
--- phase2/phase2.c
+++ phase2/phase2.c
@@ -1,5 +1,6 @@
 /* ------------------------------------------------------------------------
    phase2.c
+   Justin Kennelly & Carlos Torres
    Applied Technology
    College of Applied Science and Technology
    The University of Arizona
@@ -16,6 +17,19 @@
 /* ------------------------- Prototypes ----------------------------------- */
 int start1 (char *);
 extern int start2 (char *);
+check_kernel_mode(char *func_name);
+enableInterrupts();
+disableInterupts();
+
+int MboxCreate(int slots, int slot_size);
+int MboxSend(int mbox_id, void *msg_ptr, int msg_size);
+int MboxReceive(int mbox_id, void *msg_ptr, int msg_size);
+
+int MboxRelease(int mbox_id);
+int MboxCondSend(int mbox_id, void *msg_ptr, int msg_size);
+int MboxCondReceive(int mbox_id, void *msg_ptr, int max_msg_size);
+
+int waitdevice(int type, int unit, int *status);
 
 
 /* -------------------------- Globals ------------------------------------- */
@@ -24,18 +38,10 @@ int debugflag2 = 0;
 
 /* the mail boxes */
 mail_box MailBoxTable[MAXMBOX];
+mail_slot MailBoxSlots[MAXSLOTS];
+mbox_proc Phase2_ProcTable[MAXPROC];
 
-
-/* -------------------------- Functions -----------------------------------
-  Below I have code provided to you that calls
-
-  check_kernel_mode
-  enableInterrupts
-  disableInterupts
-  
-  These functions need to be redefined in this phase 2,because
-  their phase 1 definitions are static 
-  and are not supposed to be used outside of phase 1.  */
+/* -------------------------- Functions ----------------------------------- */
 
 /* ------------------------------------------------------------------------
    Name - start1
@@ -52,16 +58,25 @@ int start1(char *arg)
    if (DEBUG2 && debugflag2)
       console("start1(): at beginning\n");
 
-   //check_kernel_mode("start1");
+   check_kernel_mode("start1");
 
    /* Disable interrupts */
-   //disableInterrupts();
+   disableInterrupts();
 
    /* Initialize the mail box table, slots, & other data structures.
     * Initialize int_vec and sys_vec, allocate mailboxes for interrupt
     * handlers.  Etc... */
 
-   //enableInterrupts();
+   // Initialize the process table for phase 2
+   init_Table(PROC_TABLE);
+
+   //initialise mailboxtable
+   init_Table(MAILBOX_TABLE);
+
+   //initialise mailbox slots
+   init_Table(SLOT_TABLE);
+
+   enableInterrupts();
 
    /* Create a process for start2, then block on a join until start2 quits */
    if (DEBUG2 && debugflag2)
@@ -84,8 +99,13 @@ int start1(char *arg)
              mailbox id.
    Side Effects - initializes one element of the mail box array. 
    ----------------------------------------------------------------------- */
-int MboxCreate(int slots, int slot_size)
-{
+int MboxCreate(int slots, int slot_size) {
+   /* test if in kernel mode & disable interupts */
+   check_kernel_mode("MboxCreate");
+   disableInterrupts(); 
+
+
+
 } /* MboxCreate */
 
 
@@ -97,8 +117,12 @@ int MboxCreate(int slots, int slot_size)
    Returns - zero if successful, -1 if invalid args.
    Side Effects - none.
    ----------------------------------------------------------------------- */
-int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
-{
+int MboxSend(int mbox_id, void *msg_ptr, int msg_size) {
+   /* test if in kernel mode & disable interupts */
+   check_kernel_mode("MboxSend");
+   disableInterrupts(); 
+
+
 } /* MboxSend */
 
 
@@ -111,11 +135,130 @@ int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
    Returns - actual size of msg if successful, -1 if invalid args.
    Side Effects - none.
    ----------------------------------------------------------------------- */
-int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
-{
+int MboxReceive(int mbox_id, void *msg_ptr, int msg_size) {
+   /* test if in kernel mode & disable interupts */
+   check_kernel_mode("MboxReceive");
+   disableInterrupts(); 
+
+
+
 } /* MboxReceive */
 
 
 int check_io(){
-    return 0; 
-}
+
+    return 0;
+
+} /* check_io */
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< FUNCTIONS ADDED >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/* --------------------------------------------------------------------------------
+   Name - check_kernel_mode
+   Purpose - Checks to make sure functions are being called in Kernel mode
+   Parameters - *func_name keeps track of where the mode checking is being invoked.
+   Returns - nothing
+   Side Effects -  if system is in usermode, print appropriate error and halt.
+   -------------------------------------------------------------------------------- */
+static void check_kernel_mode(char *func_name) {
+   union psr_values caller_psr;  /*Holds the caller's PSR values*/
+   char buffer[200];
+
+   if (DEBUG && debugflag) {
+      sprintf(buffer, "check_kernel_mode(): called for function %s\n", func_name);
+      console("%s", buffer);
+   }
+
+   /*test if in kernel mode and halts if in user mode*/
+   caller_psr.integer_part = psr_get();
+   if(caller_psr.bits.cur_mode == 0) {
+      sprintf(buffer, "%s(): called while in user mode, process %d. Halting...\n", func_name, Current->pid);
+      console("%s", buffer);
+      halt(1);
+   }
+   
+} /* check_kernel_mode */
+
+/* ------------------------------------------------------------------------
+   Name - disableInterrupts
+   Purpose - 
+   Parameters - 
+   Returns - 
+   Side Effects -  
+   ----------------------------------------------------------------------- */
+void disableInterrupts()
+{
+  /* turn the interrupts OFF iff we are in kernel mode */
+   if((PSR_CURRENT_MODE & psr_get()) == 0) {
+      //not in kernel mode
+      console("Kernel Error: Not in kernel mode, may not disable interrupts\n");
+      halt(1);
+   } 
+   else {
+      /* We ARE in kernel mode, disable interrupts */
+      psr_set( psr_get() & ~PSR_CURRENT_INT );
+   }
+} /* disableInterrupts */
+
+/* --------------------------------------------------------------------------------
+   Name - enableInterupts
+   Purpose - Turn the interupts on
+   Parameters - none
+   Returns - nothing
+   Side Effects -  if system is in usermode, print appropriate error and halt.
+   -------------------------------------------------------------------------------- */
+void enableInterrupts() {
+   /* turn the interrupts OFF iff we are in kernel mode */
+   if((PSR_CURRENT_MODE & psr_get()) == 0) {
+      //not in kernel mode
+      console("Kernel Error: Not in kernel mode, may not enable interrupts\n");
+      halt(1);
+   } 
+   else {
+      /* We ARE in kernel mode, enable interrupts */
+      psr_set( psr_get() | PSR_CURRENT_INT );
+   }
+} /* enableInterupts */
+
+/* --------------------------------------------------------------------------------
+   Name - init_Table
+   Purpose - 
+   Parameters - 
+   Returns - 
+   Side Effects - 
+   -------------------------------------------------------------------------------- */
+void init_Table(int type) {
+   int i = 0;
+
+   switch(type) {
+      case 'MAILBOX_TABLE':
+         for (i; i < MAXMBOX; i++) {
+            MailBoxTable[i].mbox_id = -1;
+            MailBoxTable[i].status = UNUSED;
+            MailBoxTable[i].num_slots = -1;
+            MailBoxTable[i].max_slot_size = -1;
+            /* MailBoxTable[i]. */
+            /* MailBoxTable[i]. */
+            /* MailBoxTable[i]. */
+         }
+         break;
+      case 'SLOT_TABLE':
+         for (i; i < MAXSLOTS; i++) {
+            MailBoxSlots[i].mbox_id = -1;
+            MailBoxSlots[i].status = UNUSED;
+            MailBoxSlots[i].message = NULL;
+            MailBoxSlots[i].next_slot = NULL;
+         }
+         break;
+      case 'PROC_TABLE':
+         for (i; i < MAXPROC; i++) {
+            Phase2_ProcTable[i].pid = -1;
+            Phase2_ProcTable[i].next_ptr = NULL;
+         }
+         break;
+      default:
+         console("ERROR: table type was not found\n");
+         break;
+      }
+
+} /* init_Table */
