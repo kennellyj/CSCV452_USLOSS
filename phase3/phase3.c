3b8c3c0fe40c4ca4bb9c050b3b3cddfb1270ddcd
Torres7514 <97860156+Torres7514@users.noreply.github.com>
2022-03-28 23:37:33 -0700

    updated phase3.c  & sems.h
    added all functions that will used in phase 3 to get a basic layout. Organized each into it own section with definitions. Will begin to start filling each function. ProcTable was initialized but will still need to add more parameters. began to fill out some of sems.h.

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -2,12 +2,50 @@
 #include <phase1.h>
 #include <phase2.h>
 #include <phase3.h>
+#include <usyscall.h>
+#include <libuser.h>
+#include <sems.h>
+#include <string.h>
+
+/* ------------------------ PROTOTYPES ---------------------------------------------- */
 
 int start2(char *); 
+extern int start3(char *);
 int  spawn_real(char *name, int (*func)(char *), char *arg,
                 int stack_size, int priority);
 int  wait_real(int *status);
-int
+void Terminate(int status);
+
+int wait(int *pid, int *status);
+void Terminate(int status);
+void GetTimeofDay(int *tod);
+void CPUTime(int *cpu);
+void GetPID(int *pid);
+
+int  SemCreate(int value, int *semaphore);
+int  SemP(int semaphore);
+int  SemV(int semaphore);
+int  SemFree(int semaphore);
+
+
+void check_kernel_mode(char *func_name);
+
+
+
+/* ------------------------ GLOBALS ------------------------------------------------- */
+Phase3ProcTable procTable[MAXPROC];
+
+
+/* ------------------------ FUNCTIONS ----------------------------------------------- */
+
+
+/* ----------------------------------------------------------------------------------
+        Name    - start2
+        Purpose - initilize the process table, semaphore table, and sys_call vec
+        Parameters - arg: function arguments
+        Return  - 0
+        Side Effects - initializes all phase 3 structs
+    --------------------------------------------------------------------------------- */
 start2(char *arg)
 {
     int		pid;
@@ -16,10 +54,16 @@ start2(char *arg)
      * Check kernel mode here.
      */
 
+    check_kernel_mode("start2");
+
     /*
      * Data structure initialization as needed...
      */
 
+    for (int i = 0; i < MAXPROC; i++)
+    {
+        procTable[i].status = UNUSED;
+    }
 
     /*
      * Create first user-level process and wait for it to finish.
@@ -54,3 +98,145 @@ start2(char *arg)
 
 } /* start2 */
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< FUNCTIONS ADDED >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/* ----------------------------------------------------------------------------------
+        Name    - spawn
+        Purpose - creates a user-level process
+        Parameters - sysargs
+        Return  - arg1: PID of newly created process; -1 if not created
+                  arg4: -1 if illegalvalues are given as input; 0 otherwise.
+        Side Effects - spawn_real is called
+    --------------------------------------------------------------------------------- */
+void spawn (sysargs *args)
+{
+
+} /* spawn */
+
+/* ----------------------------------------------------------------------------------
+        Name    - wait
+        Purpose - waits for child process to terminate
+        Parameters - pid, status
+        Return  - arg1: process ID of terminating child
+                  arg2: the termination code of the child
+                  arg4: -1 if the process has no children, 0 otherwise
+        Side Effects - process is blocked if no terminating children
+    --------------------------------------------------------------------------------- */
+int wait(int *pid, int *status)
+{
+
+}/* wait */
+
+/* ----------------------------------------------------------------------------------
+        Name    - terminate
+        Purpose - terminate called process and all of its children
+        Parameters - status
+        Return  - NONE
+        Side Effects - Proc Status is set to UNUSED
+    --------------------------------------------------------------------------------- */
+void Terminate(int status)
+{
+
+} /* Terminate */
+
+/* --------------------------------------------------------------------------------
+   Name - SemCreate
+   Purpose - creates a user level semaphore
+   Parameters - value, *semaphore
+   Returns - arg1: semaphore handle to be used in subsequent semaphore system calls
+             arg4: -1 if initial value is negative or no more semaphores are 
+                    available, 0 otherwise
+   Side Effects -  
+   -------------------------------------------------------------------------------- */
+int SemCreate(int value, int *semaphore)
+{
+
+} /* SemCreate */
+ /* --------------------------------------------------------------------------------
+   Name - SemP
+   Purpose - performs a "P" operation on a semaphore
+   Parameters - semaphore
+   Returns - arg4: -1 if semaphore handle is invalid; 0 otherwise.
+   Side Effects -  
+   -------------------------------------------------------------------------------- */  
+int SemP(int semaphore)
+{
+
+} /* SemP */
+/* --------------------------------------------------------------------------------
+   Name - SemV
+   Purpose - performs a "V" operation on a semaphore
+   Parameters - semaphore
+   Returns - arg4: -1 if semaphore handle is invalid, 0 otherwise.
+   Side Effects -  if system is in usermode, print appropriate error and halt.
+   -------------------------------------------------------------------------------- */
+int SemV(int semaphore)
+{
+
+} /* SemV */
+/* --------------------------------------------------------------------------------
+   Name - SemFree
+   Purpose - frees a semaphore
+   Parameters - semaphore handle
+   Returns - arg4: -1 if semaphore handle is invalid.
+                    1 if there are processes blocked on the semaphore, 0 otherwise.
+   Side Effects -  terminate
+   -------------------------------------------------------------------------------- */
+int SemFree(int semaphore)
+{
+
+} /* SemFree */
+
+/* --------------------------------------------------------------------------------
+   Name - check_kernel_mode
+   Purpose - Checks to make sure functions are being called in Kernel mode
+   Parameters - *func_name keeps track of where the mode checking is being invoked.
+   Returns - nothing
+   Side Effects -  if system is in usermode, print appropriate error and halt.
+   -------------------------------------------------------------------------------- */
+void check_kernel_mode(char *func_name) {
+    char buffer[200];
+
+   if ((PSR_CURRENT_MODE & psr_get()) == 0) {
+      sprintf(buffer, "check_kernel_mode(): called for function %s\n", func_name);
+      console("%s", buffer);
+      halt(1);
+   }
+   
+} /* check_kernel_mode */
+
+/* --------------------------------------------------------------------------------
+   Name - GetTimeofDay
+   Purpose - Creturns the value of the time-of-day clock
+   Parameters - tod
+   Returns - time of day
+   Side Effects - 
+   -------------------------------------------------------------------------------- */
+void GetTimeofDay(int *tod)
+{
+
+} /* GetTimeofDay */
+
+/* --------------------------------------------------------------------------------
+   Name - CPUTime
+   Purpose - returns the CPU time of the process
+   Parameters - cpu
+   Returns - the process' CPU time
+   Side Effects -  
+   -------------------------------------------------------------------------------- */
+void CPUTime(int *cpu)
+{
+
+} /* CPUTime */
+
+/* --------------------------------------------------------------------------------
+   Name - GetPID
+   Purpose - returns the process ID of the currently running process.
+   Parameters - pid
+   Returns - Process ID
+   Side Effects -  if system is in usermode, print appropriate error and halt.
+   -------------------------------------------------------------------------------- */
+void GetPID(int *pid)
+{
+
+} /* GetPID */
--- phase3/sems.h
+++ phase3/sems.h
@@ -6,8 +6,29 @@
  * CSCV 452 Phase 3
  *
  */
+
  
  #ifndef PHASE3_HEADER_H_
  #define PHASE3_HEADER_H_
  
  //structures defs below
+
+
+// procTable status #'s
+#define UNUSED      0
+#define ACTIVE      1
+
+
+#endif /* PHASE3_HEADER_H_ */
+
+typedef struct Phase3ProcTable *ProcTablePtr;
+
+typedef struct Phase3ProcTable{
+    //add more functions here
+    char    name[MAXNAME];
+    short   pid;
+    int     priority;
+    unsigned int    stack_size;
+    int     status;
+    int     mboxID;
+}Phase3ProcTable;
